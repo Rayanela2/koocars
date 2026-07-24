877ab8ae1461b95858836ef3cca5b08ea75fb3d9
Seb <ikapi@rayan-el-arabi.home>
2026-07-24 16:21:09 +0200

    fix vercel build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
-import { CarDetail } from './car-detail/car-detail';
 import { Contact } from './contact/contact';
 import { Admin } from './admin/admin';
 
 export const routes: Routes = 
   [{ path: '', component: Homepage },
-  { path: 'voiture/:id', component: CarDetail },
   { path: 'contact', component: Contact },
   { path: 'admin', component: Admin },
 ];
