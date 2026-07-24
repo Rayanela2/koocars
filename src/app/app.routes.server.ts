155d4a8b4ed5c0419b0a150e266b48639ff7cdf0
Seb <ikapi@rayan-el-arabi.home>
2026-07-24 16:27:09 +0200

    fix vercel build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,22 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'voiture/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'contact',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'admin',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: '',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Client
   }
